a33436e588a1dc6a89340bec6315fbad88702474
Ross Hadden <rosshadden@gmail.com>
2012-12-24 21:23:16 -0500

    Organized sidebar to make room for more.

--- public/js/gui/main.js
+++ public/js/gui/main.js
@@ -1,21 +1,23 @@
-define(['text!./main.html'], function(html){
-	var $gui = $('#main');
-	var main = Handlebars.compile(html);
+define(['text!./body.html'], function(html){
+	var body = Handlebars.compile(html);
+	var $gui = $('#gui'),
+		$head = $gui.children('header'),
+		$body = $gui.children('section');
 
 	var current;
 	var gui = {
 		render: function(data){
 			current = data;
-			$gui.html(main(data));
+			$body.html(body(data));
 		},
 
 		clear: function(){
 			current = null;
-			$gui.html('');
+			$body.html('');
 		}
 	};
 
-	$gui.on('click', '.item', function(){
+	$body.on('click', '.item', function(){
 		$(gui).trigger('request', {
 			from: current.id,
 			for: $(this).attr('data-index')
